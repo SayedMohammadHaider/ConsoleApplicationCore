5db31c64713b5ae57fad86ceff8588346ba874f8
Sayed Mohammad Haider <sayed@cloudthing.com>
2019-12-26 12:33:25 +0530

    some message

---
+++ ConsoleApplicationPractice/Dictonary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationPractice
+{
+    class DictionaryTest
+    {
+        public string DictionaryPractice()
+        {
+            Dictionary<string, string> dictonary = new Dictionary<string, string>();
+            Dictionary<string, string> dictonaryDuplicate = new Dictionary<string, string>();
+            for (int i = 1; i <= 5; i++)
+            {
+                dictonary.Add(i.ToString(), RandomNumber());
+            }
+            for (int j = 1; j <= 5; j++)
+            {
+                dictonaryDuplicate.Add(j.ToString(), RandomNumber());
+            }
+
+            var duplicateList = dictonary.Where(kvp => dictonaryDuplicate.ContainsValue(kvp.Value)).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            Console.ReadLine();
+            return "";
+        }
+
+        public string RandomNumber()
+        {
+            var randomNumber = new Random();
+            return randomNumber.Next(1, 10).ToString();
+        }
+
+    }
+}
